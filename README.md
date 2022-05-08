ee0d4c158fc8f5545b4051183594fa02386fefc5
Agnieszka <s205868@sggw.edu.pl>
2022-05-08 23:22:17 +0200

    comments correction

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # szady_klasyfikacja
 Random Forest classifier with simple feature selection.
+Część projektu "Rozpoznawanie rodzaju mikroorganizmu na podstawie mikrofotografii" w ramach przedmiotu Inżynieria Oprogramowania. 
+Celem jest stworzenie aplikacji klasyfikującej bakterie na poziomie rodzaju. 
+Repozytorium zawiera implementację klasyfikatora Random Forest oraz kilka metod wyboru najistotniejszych cech (feature selection). 
+Zestaw wyodrębnionych poprzez analizę obrazu cech został umieszczony w załączonym pliku cechy.csv.
--- klasyfikacja_rf_featselect.py
+++ klasyfikacja_rf_featselect.py
@@ -12,85 +12,111 @@ from sklearn.feature_selection import RFECV
 from sklearn.model_selection import StratifiedKFold
 from sklearn.svm import SVR
 
-#import pliku z cechami
-cechy = pd.read_csv(r"cechy.csv", decimal=",", sep=";")
-X = np.array(cechy.drop(cechy.columns[[0, 1]], axis=1))
-y = np.array(cechy["-- Rodzaj --"])
-#metoda polegająca na wyborze cech PODCZAS klasyfikacji (wybieram cechy z wylosowanych do modelu trenującego)
-def klasyfikacja_wrapper():
-    print('Klasyfikacja 1 metoda')
-    #klasyfikator rf
+#import file with features
+features = pd.read_csv(r"cechy.csv", decimal=",", sep=";")
+X = np.array(features.drop(features.columns[[0, 1]], axis=1))
+y = np.array(features["-- Rodzaj --"])
+#wrapper method (evaluates on a specific machine learning algorithm to find optimal features)
+def classification_wrapper():
+    print('Classification wrapper 1 method')
     clf = RandomForestClassifier(n_estimators=100)
-    #inicjalizuje SelectFromModel, można podać jeszcze threshold oprócz klasyfikatora w parametrze
-    #threshold=0.05 - wybieram cechy których istotność w predykcji przekracza 0.05 (ja zostawiłam default)
-    najwazniejsze = SelectFromModel(clf)
-    #model treningowy: X to dataframe tylko z cechami (bez rodzaju bakterii i numeru zdjęcia), y to numery rodzajów
-    X_trenujacy, X_testowy, y_trenujacy, y_testowy = sklearn.model_selection.train_test_split(X, y, test_size=0.25)
-    #dopasowuje kalkulacje zwraca wybrane cechy
-    X_wybrane=najwazniejsze.fit_transform(X_trenujacy, y_trenujacy)
-    #wypisuje jakie i ile cech zostało wybranych (get_support() zwraca bool array rozmiaru X_train, jeśli używamy danej cechy w jej miejscu jest true, jeśli nie - jest false)
-    print('Wybrane cechy:')
-    print(cechy.columns.drop(['-- Rodzaj --', '-- Numer --'])[najwazniejsze.get_support() == True])
-    print("Liczba cech:", np.count_nonzero(najwazniejsze.get_support() == True))
-    #robię nowy model trenujący z wybranymi już "najlepszymi" cechami
-    X_trenujacy2, X_testowy2, y_trenujacy2, y_testowy2 = sklearn.model_selection.train_test_split(X_wybrane, y_trenujacy, test_size=0.25)
-    clf.fit(X_trenujacy2, y_trenujacy2)
-    #przewidywany rodzaj na podstawie danych testowych z nowego modelu, po odrzuceniu mniej istotnych cech
-    y_przwidywany = clf.predict(X_testowy2)
-    #accuracy (zgodność dopasowania)
-    print("Accuracy:", metrics.accuracy_score(y_testowy2, y_przwidywany))
-klasyfikacja_wrapper()
+
+    #initializing SelectFromModel, threshold is optional
+    #threshold=0.05 - select features with importance above 0.05
+    m_important = SelectFromModel(clf)
+
+    #train test split (X - dataframe with features, y - classes)
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.25)
+
+    #returns transformed array with selected features
+    X_selected = m_important.fit_transform(X_train, y_train)
+
+    #printing selected features (get_support() returns bool array of X_train size, if feature is selected - true)
+    print('Selected features:')
+    print(features.columns.drop(['-- Rodzaj --', '-- Numer --'])[m_important.get_support() == True])
+    print("Number of selected features:", np.count_nonzero(m_important.get_support() == True))
+
+    #train test split on already selected (most important) features and
+    X_train2, X_test2, y_train2, y_test2 = sklearn.model_selection.train_test_split(X_selected, y_train, test_size=0.25)
+    #training the model
+    clf.fit(X_train2, y_train2)
+
+    #predicting classes using trained model
+    y_predicted = clf.predict(X_test2)
+    #printing accuracy
+    print("Accuracy:", metrics.accuracy_score(y_test2, y_predicted))
+
+classification_wrapper()
 
 
 
-#metoda polegająca na wyborze cech PRZED klasyfikacją (z góry wybieram cechy przekraczające jakiś próg istotności)
-def klasyfikacja_filter():
-    print('Klasyfikacja 2 metoda')
-    #używam mutual_info_classif aby znaleźć istotność poszczególnych cech
-    istotnosc = mutual_info_classif(X,y)
-    #wypisuje ważność cech (tylko informacyjnie)
-    waznosc_cech = pd.Series(istotnosc, cechy.columns.drop(['-- Rodzaj --', '-- Numer --'])).sort_values()
-    print('Istotność cech:')
-    print(waznosc_cech)
-    #tworzę 3 przypadki
-    #1.model trenujący na niefiltrowanych cechach
-    X_trenujacy, X_testowy, y_trenujacy, y_testowy = sklearn.model_selection.train_test_split(X, y, test_size=0.25)
-    #2.model trenujący z cechami o istotności przekraczającej 0.35
-    indeksy2 = np.where(istotnosc > 0.35)[0]
-    X2 = X[:, indeksy2]
-    X_trenujacy2, X_testowy2, y_trenujacy2, y_testowy2 = sklearn.model_selection.train_test_split(X2, y, test_size=0.25)
-    # 2.model trenujący z cechami o istotności przekraczającej 0.55
-    indeksy3 = np.where(istotnosc > 0.55)[0]
-    X3 = X[:, indeksy3]
-    X_trenujacy3, X_testowy3, y_trenujacy3, y_testowy3 = sklearn.model_selection.train_test_split(X3, y, test_size=0.25)
-    #dla każdego modelu inicjalizuję klasyfikator, trenuję i przewiduję wynik na podstawie części testowej modelu
+#filter method (generic set of methods which do not incorporate a specific machine learning algorithm to find optimal features)
+def classification_filter():
+    print('Classification filter method')
+    #using mutual_info_classif to find feature importances (X - dataframe with features, y - classes)
+    f_importances = mutual_info_classif(X,y)
+
+    #printing importances for individual features
+    importances = pd.Series(f_importances, features.columns.drop(['-- Rodzaj --', '-- Numer --'])).sort_values()
+    print('Feature importances:')
+    print(importances)
+
+    #creating 3 different cases
+    #1.train test split with original dataset
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.25)
+    #2.train test split on features with importance above 0.35
+    index2 = np.where(importances > 0.35)[0]
+    X2 = X[:, index2]
+    X_train2, X_test2, y_train2, y_test2 = sklearn.model_selection.train_test_split(X2, y, test_size=0.25)
+    #3.train test split on features with importance above 0.55
+    index3 = np.where(importances > 0.55)[0]
+    X3 = X[:, index3]
+    X_train3, X_test3, y_train3, y_test3 = sklearn.model_selection.train_test_split(X3, y, test_size=0.25)
+
+    #initializing classifier for every case, training and predicting classes using trained model
     clf = RandomForestClassifier(n_estimators=100)
     clf2 = RandomForestClassifier(n_estimators=100)
     clf3 = RandomForestClassifier(n_estimators=100)
-    clf.fit(X_trenujacy, y_trenujacy)
-    clf2.fit(X_trenujacy2, y_trenujacy2)
-    clf3.fit(X_trenujacy3, y_trenujacy3)
-    y_przewidywany = clf.predict(X_testowy)
-    y_przewidywany2 = clf2.predict(X_testowy2)
-    y_przewidywany3 = clf3.predict(X_testowy3)
-    #wypisuję accuracy
-    print("Accuracy dla nieprzefiltrowanych cech:", metrics.accuracy_score(y_testowy, y_przewidywany))
-    print("Accuracy dla cech o istotności powyżej 0.35:", metrics.accuracy_score(y_testowy2, y_przewidywany2))
-    print("Accuracy dla cech o istotności powyżej 0.55:", metrics.accuracy_score(y_testowy3, y_przewidywany3))
-klasyfikacja_filter()
+    clf.fit(X_train, y_train)
+    clf2.fit(X_train2, y_train2)
+    clf3.fit(X_train3, y_train3)
+    y_predicted = clf.predict(X_test)
+    y_predicted2 = clf2.predict(X_test2)
+    y_predicted3 = clf3.predict(X_test3)
 
-def klasyfikacja_wrapper2():
-    print('Klasyfikacja 3 metoda')
-    # klasyfikator rf
+    #printing accuracy
+    print("Accuracy for 1 case (original dataset):", metrics.accuracy_score(y_test, y_predicted))
+    print("Accuracy for 2 case (> 0.35):", metrics.accuracy_score(y_test2, y_predicted2))
+    print("Accuracy for 3 case (> 0.55):", metrics.accuracy_score(y_test3, y_predicted3))
+
+classification_filter()
+
+#another wrapper method
+def classification_wrapper2():
+    print('Classification 2 wrapper method')
     clf = RandomForestClassifier(n_estimators=100)
-    rfecv=RFECV(estimator=clf, cv=3)
-    X_wybrane=rfecv.fit_transform(X,y)
-    print("Optymalna liczba cech:", format(rfecv.n_features_))
-    print(cechy.columns.drop(['-- Rodzaj --', '-- Numer --'])[rfecv.get_support() == True])
-    X_trenujacy, X_testowy, y_trenujacy, y_testowy=sklearn.model_selection.train_test_split(X_wybrane, y, test_size=0.25)
-    clf.fit(X_trenujacy, y_trenujacy)
-    y_przwidywany = clf.predict(X_testowy)
-    print("Accuracy:", metrics.accuracy_score(y_testowy, y_przwidywany))
-klasyfikacja_wrapper2()
+
+    #initializing rfecv (ecursive feature elimination with cross-validation to select the number of features)
+    rfecv = RFECV(estimator=clf, cv=3)
+
+    #returns transformed array with selected features
+    X_selected = rfecv.fit_transform(X,y)
+
+    #printing optimal number of features for individual prediction and selected features itself
+    print("Optimal number of features:", format(rfecv.n_features_))
+    print(features.columns.drop(['-- Rodzaj --', '-- Numer --'])[rfecv.get_support() == True])
+
+    #train test split on already selected (most important) features
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_selected, y, test_size=0.25)
+    #training the model
+    clf.fit(X_train, y_train)
+
+    #predicting classes using trained model
+    y_predicted = clf.predict(X_test)
+
+    #printing accuracy
+    print("Accuracy:", metrics.accuracy_score(y_test, y_predicted))
+
+classification_wrapper2()
 
 
